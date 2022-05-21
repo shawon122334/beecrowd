d9a123231147b23496f286989a5c68b37625dae8
sabbir <shawon.mu11@gmail.com>
2022-05-22 02:13:03 +0600

    bank notes and coins

--- 1021.py
+++ 1021.py
@@ -1,11 +1,13 @@
 n = float(input())
+
 print('NOTAS:')
+
 for i in [100,50,20,10,5,2]:
-    x = n//i
-    print(f"{int(x)} nota(s) de R$ {i},00")
-    n = n%i
+    print(f"{int(n/i)} nota(s) de R$ {i}.00")
+    n = float(f"{n%i:.2f}")
+
 print('MOEDAS:')
-for j in [1,0.50,0.25,0.10,0.05,0.01]:
-    y = n//j
-    print(f"{int(y)} moeda(s) de R$ {j:.2f}")
-    n = n%j
+
+for i in [1,0.50,0.25,0.10,0.05,0.01]:
+    print(f"{int(n/i)} moeda(s) de R$ {i:.2f}")
+    n = float(f"{n%i:.2f}")
